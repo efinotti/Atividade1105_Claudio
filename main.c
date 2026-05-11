2520c451ea4e66fc3b0209b844cdbd0d8e45c9ce
arthurrodriguessdev <arthurdevcontatoo@gmail.com>
2026-05-11 20:17:48 -0300

    inicializando estruturas

--- main.c
+++ main.c
@@ -1,1 +1,91 @@
 #include <stdio.h>
+#define QUANTIDADE_INGRESSOS 1000
+#define ALUNO 1
+#define COMUNIDADE 2
+#define IDOSO 3
+
+// Comidas e Bebidas
+typedef struct Bebida{
+    char nome[100];
+    float preco;
+    int quantidadeEstoque;
+} Bebida;
+
+// Entidades
+typedef struct Aluno{
+    char nome[100];
+    float credito;
+} Aluno;
+
+typedef struct Idoso{
+    char nome[100];
+} Idoso;
+
+typedef struct Comunidade{
+    char nome[100];
+} Comunidade;
+
+// Filas
+typedef struct FilaPessoas{
+    int id;
+    char nome[100];
+} FilaPessoas;
+
+typedef struct FilaGeral{
+    FilaPessoas idosos;
+    FilaPessoas comunidade;
+    FilaPessoas alunos;
+} FilaGeral;
+
+
+int comprar_creditos(){
+
+}
+
+int main(){
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* int gerarRegistros() {
+    FILE* f = fopen("participantes.csv", "w");
+    if (!f) return 0;
+    srand(time(NULL));
+
+    for (int i = 0; i < MAX; i++) {
+        int comida = rand() % 3 + 1;
+        int bebida = rand() % 5 + 1;
+        int quantidadeEstoque = rand() % 3 + 1;
+
+        fprintf(f, "%d;%d;%d;%d\n", i + 1, carne, bebida, musica);
+    }
+
+    fclose(f);
+    return 1;
+} */
